6b1716a4d233e8ee2368d304c76db7cf9aaf7767
Shivam <108368317+shivpatel5583@users.noreply.github.com>
2025-03-15 10:05:16 -0400

    movement

--- godot-game/player.gd
+++ godot-game/player.gd
@@ -1,11 +1,44 @@
-extends Node
+extends CharacterBody2D
+@export var speed = 400
+var screen_size
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	screen_size = get_viewport_rect().size
+	#hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$sprite.play()
+	else:
+		$sprite.stop()
+	
+	if velocity.x != 0:
+		$sprite.animation = "walk"
+		$sprite.flip_v = false
+		$sprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$sprite.animation = "up"
+		$sprite.flip_v = true
+		$sprite.flip_v = velocity.y > 0
+	
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+		
+		
+		
